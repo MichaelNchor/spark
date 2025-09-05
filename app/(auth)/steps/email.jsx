6af369270e2ee4ee9cc2000ff61f04aea12ce4f5
Michael Nchor <michael.nchor@hubtel.com>
2025-09-05 17:15:23 +0000

    add email screen

---
+++ app/(auth)/steps/email.jsx
@@ -0,0 +1,61 @@
+import { router } from "expo-router";
+import React, { useState } from "react";
+import { SafeAreaView, ScrollView, Text, View } from "react-native";
+import CustomButton from "../../../components/CustomButton";
+import InputField from "../../../components/InputField";
+import { useStep } from "../../../state/StepContext";
+
+const Email = () => {
+  const { setStep } = useStep();
+  const [form, setEmail] = useState({
+    Email: "",
+  });
+
+  return (
+    <SafeAreaView className="bg-white flex-1">
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1,
+          paddingTop: 60,
+          paddingHorizontal: 30,
+          justifyContent: "space-between",
+          paddingBottom: 48, // same as pb-12
+        }}
+      >
+        {/* Content */}
+        <View className="w-full gap-4">
+          <Text className="font-poppins-medium text-4xl mb-2">My email</Text>
+
+          <Text className="font-poppins-regular text-gray-500">
+            Please enter your email. We will send you a 4-digit
+            code to verify your account.
+          </Text>
+
+          <View className="w-full">
+            <InputField
+              value={form.Email}
+              handleChangeText={(e) => {
+                setEmail({ ...form, Email: e });
+              }}
+              keyboardType="phone-pad"
+            />
+          </View>
+        </View>
+
+        {/* Fixed bottom button */}
+        <View className="w-full">
+          <CustomButton
+            text="Continue"
+            handlePress={() => {
+              setStep(4);
+              router.push("/steps/verify-code");
+            }}
+            containerStyles="w-full h-[48px]"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Email;
